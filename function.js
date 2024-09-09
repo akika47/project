c7e7e8e7f10a2a9ab39aa4e73b9eb0a7c5cfe90f
otvos.akos <akika02531@gmail.com>
2024-09-09 14:29:13 +0200

    Util function factorial

--- function.js
+++ function.js
@@ -5,3 +5,14 @@ function getFirstElement (arr){
     const words = ["apple", "banana&quot","cherry&quot"];
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
+
+    function factorial(n) {
+        let ans = 1; 
+    
+        if(n === 0)
+            return 1;
+        for (let i = 2; i <= n; i++) 
+            ans = ans * i; 
+        return ans; 
+    }
+        console.log(`Factorial of 5: ${factorial(5)}`);
