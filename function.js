abf00d9341cc641b284a8e369d8b53968e01d0c5
otvos.akos <akika02531@gmail.com>
2024-09-09 14:24:49 +0200

    improvement

--- function.js
+++ function.js
@@ -1,7 +1,10 @@
 function getFirstElement (arr){
+    if (arr.length == 0) {
+        return "üres tömb"
+    }
         return arr[0]
     }
-    const numbers = [1, 2, 3];
+    const numbers = [];
     const words = ["apple", "banana&quot","cherry&quot"];
     console.log(`First number: ${getFirstElement(numbers)}`);
     console.log(`First word: ${getFirstElement(words)}`);
